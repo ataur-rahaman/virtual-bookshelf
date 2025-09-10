ef7f21b9152f73b6ad5e3ad5975136daf54d2114
Ataur Rahaman <ataurfb4445@gmail.com>
2025-09-10 16:32:46 +0600

    added add book page

--- src/components/SliderTwo.jsx
+++ src/components/SliderTwo.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const SliderTwo = () => {
   return (
     <div className="bg-gradient-to-br from-sky-50 to-sky-50 dark:from-slate-900 dark:to-slate-900 py-16 px-4 sm:px-8 lg:px-16">
-      <div className="max-w-6xl mx-auto flex flex-col-reverse lg:flex-row items-center gap-10">
+      <div className="max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-center gap-10">
         <div className="w-full lg:w-1/2">
           <h1 className="text-4xl sm:text-5xl font-extrabold text-slate-900 dark:text-white mb-6 leading-tight">
             Your Books, Organized{" "}
-            <span className="text-sky-600 dark:text-sky-400">
+            <span className="text-blue-600 dark:text-blue-400">
               Effortlessly.
             </span>
           </h1>
@@ -16,7 +16,7 @@ const SliderTwo = () => {
             Tired of scattered book lists? Build your own digital bookshelf to track what you've read, what you're reading, and what's next — all in one clean, user-friendly space.
           </p>
         </div>
-        <div className="w-full lg:w-1/2 flex justify-center">
+        <div className="w-full lg:w-1/2 lg:flex justify-center hidden">
           <img
             src="https://cdn-icons-png.flaticon.com/512/3861/3861717.png"
             alt="Bookshelf illustration"
--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { use } from "react";
+import { AuthContext } from "../contexts/AuthContext/AuthContext";
+
 
 const AddBook = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user} = use(AuthContext);
+  return (
+    <>
+    <title>Add Book</title>
+    <div className="min-h-screen flex justify-center items-center bg-sky-50 py-[50px] md:py-[100px]">
+      <form className="bg-base-200 border-base-300 rounded-[20px] w-xl border p-4 shadow-2xl">
+        <legend className="md:text-3xl text-blue-600 dark:text-white font-bold text-center mb-3">Add Book</legend>
+
+            <fieldset className="fieldset">
+              <label className="label ml-3">Book Title</label>
+              <input type="text" name="book_title" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" placeholder="write the book title here" />
+
+              <label className="label ml-3">Cover Photo</label>
+              <input type="text" name="cover_photo" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" placeholder="paste your book cover image URL here" />
+
+              <label className="label ml-3">Total Page</label>
+              <input type="number" name="total_page" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" placeholder="how many pages in this book?" />
+
+              <label className="label ml-3">Book Author</label>
+              <input type="text" name="book_author" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" placeholder="write here the author name" />
+
+              <label className="label ml-3">User Email</label>
+              <input type="email" name="user_email" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" defaultValue={user.email} readOnly />
+
+              <label className="label ml-3">User Name</label>
+              <input type="text" name="user_name" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" defaultValue={user.displayName} readOnly />
+
+              <label className="label ml-3">Book Category</label>
+              <select defaultValue="Select a category" name='book_category' className="shadow-md select w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]">
+                    <option disabled={true}>Select a category</option>
+                    <option>Fiction</option>
+                    <option>Non-Fiction</option>
+                    <option>Fantasy</option>
+              </select>
+
+              <label className="label ml-3">Reading Status</label>
+              <select defaultValue="Select a status" name='reading_status' className="shadow-md select w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]">
+                    <option disabled={true}>Select a status</option>
+                    <option>Read</option>
+                    <option>Reading</option>
+                    <option>Want to Read</option>
+              </select>
+
+              <label className="label ml-3">Book Overview</label>
+              <input type="text" name="book_overview" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" placeholder="write the book overview" />
+
+              <label className="label ml-3">Book Upvote</label>
+              <input type="number" name="upvote" className="shadow-md input w-full mb-3 focus:border-blue-500 focus:outline-blue-500 rounded-[50px]" defaultValue={0} readOnly />
+            </fieldset>
+            <div className="flex justify-center">
+                <input type="submit" value="Submit" className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl rounded-[50px] bg-blue-500 hover:bg-blue-600 text-white" />
+            </div>
+      </form>
+    </div>
+    </>
+  );
 };
 
 export default AddBook;
